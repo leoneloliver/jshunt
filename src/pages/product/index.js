40a0b7642af3e60ec6d569de23ca90eb284afa5b
Leo Oliveira <leooliveira@DMZs-MacBook-Air.local>
2020-07-15 20:54:06 -0400

    updates

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-
 import './styles.css';
-
-
-
 export default class Product extends Component {
     state = {
         product: {},
@@ -12,16 +8,13 @@ export default class Product extends Component {
 
     async componentDidMount(){
         const { id } = this.props.match.params;
-
         const response = await api.get(`/products/${id}`);
-
         this.setState({ product: response.data });
     }
     render() {
         const { product } = this.state;
 
         return (
-
             <div className="product-info">
                 <article>
                     <h1>{product.title}</h1>
